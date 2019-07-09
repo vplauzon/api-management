26b0463bbb98c1c95071ce35cc0943c35bec77d0
vplauzon <vplauzon@vplauzon.com>
2019-07-09 12:56:17 -0400

    Add JWT validation

--- private-public/deploy.sh
+++ private-public/deploy.sh
@@ -8,14 +8,20 @@
 ##  1- Name of resource group
 ##  2- Organization Name
 ##  3- Admin email
+##  4- Tenant Name
+##  5- Application ID
 
 rg=$1
 org=$2
 admin=$3
+tenantName=$4
+appId=$5
 
 echo "Resource group:  $rg"
 echo "Organization Name:  $org"
 echo "Admin email:  $admin"
+echo "Tenant Name:  $tenantName"
+echo "Application ID:  $appId"
 
 echo
 echo "Deploying ARM template"
@@ -24,5 +30,7 @@ az group deployment create -n "deploy-$(uuidgen)" -g $rg \
     --template-file deploy.json \
     --parameters \
     organizationName=$org \
-    adminEmail=$admin
+    adminEmail=$admin \
+    tenantName=$tenantName \
+    appId=$appId
 
