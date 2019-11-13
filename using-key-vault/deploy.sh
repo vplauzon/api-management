4d9d3f31dc951cba5ab79c26c4672f1bee8b72a7
vplauzon <vplauzon@vplauzon.com>
2019-11-13 16:00:17 -0500

    Give service a unique name

--- using-key-vault/deploy.sh
+++ using-key-vault/deploy.sh
@@ -6,13 +6,18 @@
 ##  Takes 2 parameters:
 ##
 ##  1- Name of resource group
+##  2- API-M Service name
 
 rg=$1
+service=$2
 
 echo "Resource group:  $rg"
+echo "Service name:  $service"
 
 echo
 echo "Deploying ARM template"
 
 az group deployment create -n "deploy-$(uuidgen)" -g $rg \
-    --template-file deploy.json
+    --template-file deploy.json \
+    --parameters \
+    serviceName=$service
